b10dc8b1b6d19d7c1d72420368d388226ef14c40
Sehyeon Kim <sue2368@gmail.com>
2022-01-04 15:42:22 +0900

    test commit

--- pages/form/create.js
+++ pages/form/create.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import {v4 as uuidv4} from 'uuid'
 
 export default function Create() {
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState([]) 
 
   class SelectOption {
     constructor() {
